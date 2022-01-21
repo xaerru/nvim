c2beffddcb79cd270a1a3a600c1665895932f143
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-22 00:08:18 +0530

    feat: add autosave.nvim

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ require('packer').startup(function()
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'ludovicchabant/vim-gutentags' -- Automatic tags management
   -- UI to select things (files, grep results, open buffers...)
+  use 'Pocco81/AutoSave.nvim'
   use 'akinsho/bufferline.nvim'
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
@@ -50,4 +51,7 @@ require('plugins.bufferline').load()
 require('plugins.gitsigns').load()
 require('plugins.treesitter').load()
 require('plugins.whichkey').load()
+require('autosave').setup({
+  execution_message=''
+})
 -- vim: ts=2 sts=2 sw=2 et
