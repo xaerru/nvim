1b8126d8a18fef499b3d8af55133daa2c8d74833
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-22 01:55:32 +0530

    refactor: init.lua and load options and keybinds before plugins

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@ local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.n
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
-require("plugins")
 
-local options = require("options")
-options.load()
+require('options').load()
 require("keybinds").load()
+require("plugins")
