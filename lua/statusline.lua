4ed32d942e8c01f0a46f06a7f067ea1d18d42b3c
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-21 23:41:13 +0530

    feat(statusline): add some stuff

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,8 +1,34 @@
 local generator = function()
+    local builtin = require('el.builtin')
+    local sections = require('el.sections')
     local extensions = require('el.extensions')
     local subscribe = require('el.subscribe')
+    local helper = require('el.helper')
     local segments = {
         extensions.mode,
+        " ",
+        extensions.git_branch,
+        " ",
+        { builtin.file_relative },
+        { sections.split },
+        { subscribe.buf_autocmd("el_git_changes", "BufWritePost", function(window, buffer)
+             return extensions.git_changes(window, buffer)
+         end)},
+        { "[" },
+        { builtin.line_with_width(3) },
+        { ":" },
+        { builtin.column_with_width(2) },
+        { "]" },
+        {
+        sections.collapse_builtin {
+          "[",
+          builtin.help_list,
+          builtin.readonly_list,
+          "]",
+          },
+        },
+        { builtin.filetype },
+
     }
     return segments
 end
