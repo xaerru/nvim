a60a4bf9d4f6c1df602d0a70a4794b47b5b58f48
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-21 21:51:50 +0530

    remove: kickstart options

--- init.lua
+++ init.lua
@@ -42,36 +42,6 @@ end)
 local options = require("options")
 options.load()
 
---Set highlight on search
-vim.o.hlsearch = false
-
---Make line numbers default
-vim.wo.number = true
-
---Enable mouse mode
-vim.o.mouse = 'a'
-
---Enable break indent
-vim.o.breakindent = true
-
---Save undo history
-vim.opt.undofile = true
-
---Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
---Decrease update time
-vim.o.updatetime = 250
-vim.wo.signcolumn = 'yes'
-
---Set colorscheme
-vim.o.termguicolors = true
-vim.cmd [[colorscheme base16-default-dark]]
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
 --Set statusbar
 require('lualine').setup {
   options = {
