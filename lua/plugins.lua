1099c672ddab3f36dfd31d6d6076efcef4b2a580
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-22 01:50:31 +0530

    feat: move most of the config to plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,7 @@ local use = require("packer").use
 require("packer").startup(function()
     use({ "wbthomason/packer.nvim" }) -- Package manager
     use({ "numToStr/Comment.nvim" }) -- "gc" to comment visual regions/lines
+    use({ "nvim-lua/plenary.nvim" })
     use({
         "Pocco81/AutoSave.nvim",
         config = function()
@@ -17,13 +18,11 @@ require("packer").startup(function()
             require('plugins.autopairs').load()
         end
     })
-    use({ "akinsho/bufferline.nvim" })
     use({
         "nvim-telescope/telescope.nvim",
         cmd = "Telescope",
         module = 'telescope.finders',
         requires = { 
-            "nvim-lua/plenary.nvim",
             {
                 "nvim-telescope/telescope-fzf-native.nvim", run = "make"
             }
@@ -32,15 +31,52 @@ require("packer").startup(function()
             require('plugins.telescope')
         end
     })
-    use({ "RRethy/nvim-base16" })
-    use({ "tjdevries/express_line.nvim", requires = { "lewis6991/gitsigns.nvim" } })
-    -- Add indentation guides even on blank lines
-    use({ "lukas-reineke/indent-blankline.nvim" })
-    use({ "folke/which-key.nvim" })
-    -- Add git related info in the signs columns and popups
-    use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
-    -- Highlight, edit, and navigate code using a fast incremental parsing library
-    use({ "nvim-treesitter/nvim-treesitter" })
-    -- Additional textobjects for treesitter
+    use({
+        "RRethy/nvim-base16",
+        event = "VimEnter",
+        config = function()
+            vim.cmd([[ colorscheme base16-default-dark ]])
+        end
+    })
+    use({
+        "tjdevries/express_line.nvim",
+        requires = { "lewis6991/gitsigns.nvim" },
+        after = "nvim-base16",
+        config = function()
+            require('statusline')
+        end
+    })
+    use({
+        "akinsho/bufferline.nvim",
+        after = "express_line.nvim",
+        config = function()
+            require('plugins.bufferline').load()
+        end
+    })
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        after = 'plenary.nvim',
+        config = function()
+            require('plugins.treesitter').load()
+        end
+    })
     use({ "nvim-treesitter/nvim-treesitter-textobjects" })
+    use({
+        "folke/which-key.nvim",
+        after = "bufferline.nvim",
+        config = function()
+            require('plugins.whichkey').load()
+        end
+    })
+    use({
+        "lewis6991/gitsigns.nvim",
+        after = "plenary.nvim",
+        config = function()
+            require('plugins.gitsigns').load()
+        end
+    })
+    use({
+        "lukas-reineke/indent-blankline.nvim",
+        after = "nvim-base16"
+    })
 end)
