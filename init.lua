9d2a24990b27a2f30db18833842d2d7fde5cce55
Gauravsingh Sisodia <xaerru@disroot.org>
2022-01-21 21:54:44 +0530

    remove: fugitive plugins

--- init.lua
+++ init.lua
@@ -15,8 +15,6 @@ vim.cmd [[
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
-  use 'tpope/vim-fugitive' -- Git commands in nvim
-  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'ludovicchabant/vim-gutentags' -- Automatic tags management
   -- UI to select things (files, grep results, open buffers...)
